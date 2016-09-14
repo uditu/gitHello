a4ae2f0d68d8337bc338e2305f0a01f6c6f4266a
Ujagat 1 <Ujagat 1@Ujagar>
2016-09-14 21:49:28 +0530

    First Git project

---
+++ gitHello/src/com/home/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.home;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git World");
+
+	}
+
+}
